d13e6fe5ab5abec3afdd098225fa0c3220cd31dc
sary112002 <sme112002@yahoo.com>
2024-05-20 01:40:31 +0300

    initial commit

---
+++ invoice-system-php/dbcon.php
@@ -0,0 +1,9 @@
+<?php
+$con = mysqli_connect("localhost","u346403366_purchases",";"/n=G3=0Wf","u346403366_purchases");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+?>
